c593a270445c628c6401b1ab6a211a438adc0f5c
Ilya Shikhovtsev <shikhovtsev97@gmail.com>
2018-10-16 12:40:48 +0500

    added scripts for meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date_time   TIMESTAMP,
+  description VARCHAR,
+  calories    INTEGER,
+  user_id     INTEGER NOT NULL,
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT meal_date_unique UNIQUE (date_time, user_id)
+)
